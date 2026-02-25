5dcea7efd02a5b4b719e2bae369f638098a56f6f
Andy-home <furyolo@163.com>
2026-02-25 12:00:56 +0800

    remove temp script

--- _setup_repo.ps1
+++
@@ -1,5 +0,0 @@
-Set-Location "D:\Coding\daily-update"
-git init
-git add -A
-git commit -m "initial commit"
-gh repo create daily-update --public --description "Windows 11 daily package manager update toolkit" --source=. --remote=origin --push
